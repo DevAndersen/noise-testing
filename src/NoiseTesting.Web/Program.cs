4a5799356d67b1f5d9d730b8379d5b2a8984dd35
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-05-29 10:46:28 +0200

    Added web project

---
+++ src/NoiseTesting.Web/Program.cs
@@ -0,0 +1,23 @@
+using NoiseTesting.Web.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
