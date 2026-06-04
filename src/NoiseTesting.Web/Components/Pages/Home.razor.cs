07f0cedcd5bb236d81c35e1acd45e48aa18594a0
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-06-04 15:51:52 +0200

    WIP rework main into map explorer

--- src/NoiseTesting.Web/Components/Pages/Home.razor.cs
+++ src/NoiseTesting.Web/Components/Pages/Home.razor.cs
@@ -12,6 +12,36 @@ public partial class Home
     private static readonly PerlinNoiseGenerator _temperatureNoiseGenerator = new PerlinNoiseGenerator(5);
     private static readonly PerlinNoiseGenerator _riverNoiseGenerator = new PerlinNoiseGenerator(7);
 
+    private int PosX { get; set; }
+
+    private int PosY { get; set; }
+
+    private int Width { get; set; }
+
+    private int Height { get; set; }
+
+    private int Seed { get; set; }
+
+    private static readonly Dictionary<ImageDetails, byte[]> _images = [];
+
+    protected override void OnInitialized()
+    {
+        Width = 2;
+        Height = 2;
+    }
+
+    private byte[] GetImageBytes(int posX, int posY)
+    {
+        ImageDetails details = new ImageDetails(posX, posY, Seed);
+
+        if (!_images.TryGetValue(details, out byte[]? bytes))
+        {
+            bytes = _images[details] = GenerateTerrainImages(256, posX * 256, posY * 256);
+        }
+
+        return bytes;
+    }
+
     private static byte[] GenerateTerrainImages(int size, int posX, int posY)
     {
         float[,] terrainNoise = _terrainNoiseGenerator.GenerateNoise(size, 64, 5, posX, posY, f => Sigmoid(f, 2.5F, 0.0F));
@@ -28,4 +58,26 @@ public partial class Home
 
         return ImageGenerator.Generate(color, (x, y, value) => TerrainTemperatureColor(value, temperatureNoise[x, y], riverNoise[x, y]));
     }
+
+    private void MoveUp()
+    {
+        PosY--;
+    }
+
+    private void MoveDown()
+    {
+        PosY++;
+    }
+
+    private void MoveLeft()
+    {
+        PosX--;
+    }
+
+    private void MoveRight()
+    {
+        PosX++;
+    }
+
+    private readonly record struct ImageDetails(int PosX, int PosY, int Seed);
 }
